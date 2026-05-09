6b65597fdf6a1f2267e6755f7bd7eecc86764717
MaVirgil <magnusvirgil@hotmail.com>
2026-05-09 23:14:09 +0200

    adjust colors

--- src/components/PointIndicatorDisplay.tsx
+++ src/components/PointIndicatorDisplay.tsx
@@ -15,7 +15,7 @@ function PointIndicatorDisplay({remainingPoints} : {remainingPoints : number}) {
         <div className={'flex flex-col items-center gap-1'}>
             <div className={'flex justify-between gap-0.5'}>
                 {pointDisplayData.map((isSpent : boolean, index : number) =>
-                    <div key={index} className={`rounded-sm ${isSpent ? 'bg-red-600' : 'bg-blue-600'} w-8 h-3.5`}></div>
+                    <div key={index} className={`rounded-sm ${isSpent ? 'bg-warning' : 'bg-primary'} w-8 h-3.5`}></div>
                 )}
             </div>
             <div>
--- src/components/PointSection.tsx
+++ src/components/PointSection.tsx
@@ -7,7 +7,7 @@ function PointSection( {remainingPoints} : {remainingPoints: number}) {
 
 
     return (
-        <div className={'flex flex-col gap-4 bg-gray-100 w-full p-4'}>
+        <div className={'flex flex-col gap-4 bg-background w-full p-4 border-b-2 border-b-gray-200'}>
             <div className={'flex justify-center'}>
                 <div className={'flex flex-col items-center'}>
                     <div className={'flex gap-1 text-gray-600'}>
