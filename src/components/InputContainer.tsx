1af2c1ac7184694f551dca5b8c1698dd6ece7e5b
MaVirgil <magnusvirgil@hotmail.com>
2026-05-11 00:05:36 +0200

    fix code style

--- src/components/InputContainer.tsx
+++ src/components/InputContainer.tsx
@@ -56,7 +56,16 @@ function InputContainer({callback, guessList, actorNames}: {
                         placeholder={'Guess...'}
                         onChange={event => handleSearchInputChange(event)}
                         value={value}
-                        className={`w-6/7 border-2 rounded-t-lg ${searchItems.length <= 0 ? 'rounded-b-lg' : ''} p-3 border-gray-200 outline-none focus:border-primary`}
+                        className={
+                            `w-6/7
+                            border-2
+                            rounded-t-lg
+                            ${searchItems.length <= 0 ? 'rounded-b-lg' : ''}
+                            p-3
+                            border-gray-200
+                            outline-none
+                            focus:border-primary`
+                        }
                     />
                     <button type={'submit'} className={'bg-primary rounded-lg text-white w-1/9 cursor-pointer'}>↵</button>
                 </div>
@@ -64,12 +73,22 @@ function InputContainer({callback, guessList, actorNames}: {
 
             {searchItems.length > 0 &&
                 <div className={'p-2 border-2 border-t-0  rounded-b-lg w-6/7 border-gray-200'}>
-                    {searchItems.map((item) => <p onClick={() => handleSearchSuggestionClick(item)} className={'cursor-pointer hover:text-shadow-lg'} key={item}>{item}</p>)}
+                    {searchItems.map(
+                        (item) =>
+                            <p onClick={() => handleSearchSuggestionClick(item)} className={'cursor-pointer hover:font-bold'} key={item}>
+                                {item}
+                            </p>
+                    )}
                 </div>
             }
 
             <div className={'flex flex-wrap gap-3 mt-2'}>
-                {guessList.map((guess, index) => <div key={index} className={'bg-red-200 text-warning pl-4 pr-4 pt-1 pb-1 text-center rounded-2xl'}>• {guess} <p className={'inline text-warning font-thin '}>-{WRONG_GUESS_COST}</p></div>)}
+                {guessList.map(
+                        (guess, index) =>
+                            <div key={index} className={'bg-red-200 text-warning pl-4 pr-4 pt-1 pb-1 text-center rounded-2xl'}>
+                                • {guess}<p className={'inline text-warning font-thin '}>-{WRONG_GUESS_COST}</p>
+                            </div>
+                )}
             </div>
         </div>
     );
